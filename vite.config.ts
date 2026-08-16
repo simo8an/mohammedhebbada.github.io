7af7b3701728f113d95cd93264e654867c353fbf
simo8an <Mohammed.Hebbada@emsi-edu.ma>
2026-08-16 01:57:18 +0100

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/mohammedhebbada.github.io/',
   plugins: [react(), tailwindcss()],
 })
